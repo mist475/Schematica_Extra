b66cd6747c2a3bbcb925996f21b4c88d6dc0864a
bombcar <github@bombcar.com>
2022-02-09 16:15:53 -0600

    revert

--- src/main/java/com/github/lunatrius/schematica/reference/Reference.java
+++ src/main/java/com/github/lunatrius/schematica/reference/Reference.java
@@ -4,8 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Reference {
-    public static final String MODID = "GRADLETOKEN_MODID";
-    public static final String NAME = "GRADLETOKEN_MODNAME";
+    public static final String MODID = "Schematica";
+    public static final String NAME = "Schematica";
     public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String DEPENDENCIES = "after:LunatriusCore;";
     public static final String PROXY_SERVER = "com.github.lunatrius.schematica.proxy.ServerProxy";
